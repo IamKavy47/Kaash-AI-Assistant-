2634474eb8bb17216e48c83ad1ef8effe17109c4
Kavy Porwal <159620541+IamKavy47@users.noreply.github.com>
2025-01-06 08:59:52 +0000

    Automation cmplt

--- Backend/Automation.py
+++ Backend/Automation.py
@@ -52,7 +52,7 @@ professional_responses = [
 
 messages = []
 
-SystemChatBot = {"role": "system", "content": f"Hello, I am {os.environ['Username']}, You're a content writer. You have to write content like letter"}
+SystemChatBot = [{"role": "system", "content": f"Hello, I am {os.environ['Username']}, You're a content writer. You have to write content like letters, codes, application , essays, notes,songs,poems etc."}]
 
 def GoogleSearch(Topic):
     search(Topic)
@@ -88,13 +88,148 @@ def Content(Topic):
         return Answer
     
     Topic: str=Topic.replace("Content", "")
-    ContentByAI = ContentWritterAI(Topic)
+    ContentByAI = ContentWriterAI(Topic)
 
     with open(rf"Data\{Topic.lower().replace(' ','')}.txt", "w", encoding="utf-8") as file:
         file.write(ContentByAI)
         file.close()
 
-    OpenNotpade(rf"Data\{Topic.lower().replace(' ','')}.txt")
+    OpenNotepad(rf"Data\{Topic.lower().replace(' ','')}.txt")
     return True
 
-def
+def YoutubeSearch(Topic):
+    Url4Search = f"https://www.youtube.com/results?search_query={Topic}"
+    webbrowser.open(Url4Search)
+    return True
+
+def PlayYoutube(query):
+    playonyt(query)
+    return True
+
+def OpenApp(app, sess=requests.session()):
+
+    try:
+        appopen(app, match_closest=True, output=True, throw_error=True)
+        return True
+    
+    except:
+        def extract_link(html):
+            if html is None:
+                return []
+            soup = BeautifulSoup(html, 'html.parser')
+            links = soup.find_all('a', {'jsname': 'UWckNb'})
+            return [link.get('href') for link in links]
+
+        def search_google(query):
+            url = f"https://www.google.com/search?q={query}"
+            headers = {"User-Agent": useragent}
+            response = sess.get(url, headers=headers)
+
+            if response.status_code == 200:
+                return response.text
+            else:
+                print("Failed to retrieve search results.")
+            return None
+
+        html = search_google(app)
+
+        if html:
+            link = extract_link(html)[0]
+            webopen(link)
+
+        return True
+
+def CloseApp(app):
+    
+    if "chrome" in app:
+        pass
+    else:
+        try:
+            close(app, match_closest=True, output=True,throw_error= True)
+            return True
+        except:
+            return False
+        
+def System(command):
+
+    def mute():
+        keyboard.press_and_release("volume mute")
+    
+    def unmute():
+        keyboard.press_and_release("volume mute")
+
+    def volume_up():
+        keyboard.press_and_release("volume up")
+    
+    def volume_down():
+        keyboard.press_and_release("volume down")
+    
+    if command == "mute":
+        mute()
+    elif command == "unmute":
+        unmute()
+    elif command == "volume up":
+        volume_up()
+    elif command == "volume down":
+        volume_down()
+    
+    return True
+
+async def TranslateAndExecute(commands: list[str]):
+
+    funcs = []
+
+    for command in commands:
+
+        if command.startswith("open "):
+            
+            if "open it" in command:
+                pass
+            if "open file" in command:
+                pass
+            else:
+                fun = asyncio.to_thread(OpenApp, command.removeprefix("open "))
+                funcs.append(fun)
+        
+        elif command.startswith("general "):
+            pass
+        elif command.startswith("realtime "):
+            pass
+        elif command.startswith("close "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("close "))
+            funcs.append(fun)
+        elif command.startswith("play "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("play "))
+            funcs.append(fun)
+        elif command.startswith("content "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("content "))
+            funcs.append(fun)
+        elif command.startswith("google search "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("google search "))
+            funcs.append(fun)
+        elif command.startswith("youtube search "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("youtube search "))
+            funcs.append(fun)
+        elif command.startswith("system "):
+            fun = asyncio.to_thread(CloseApp, command.removeprefix("system "))
+            funcs.append(fun)
+        else:
+            print(f"No function found for {command}")
+        
+    results = await asyncio.gather(+funcs)
+
+    for result in results :
+        if isinstance(result, str):
+            yield result
+        else:
+            yield result
+
+async def Automation(commands: list[str]):
+
+    async for result in TranslateAndExecute(commands):
+        pass
+    
+    return True
+
+if __name__ == "__main__":
+    asyncio.run(Automation(["open youtube", "open youtube", "play afsanay"]))
